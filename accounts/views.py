0c1846de15165dc52705139ef5657dc1d73fe1f9
unknown <nafla005@gmail.com>
2023-10-02 20:00:38 +0530

    first

---
+++ accounts/views.py
@@ -0,0 +1,44 @@
+from django.contrib import messages
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from . models import *
+from . models import logn
+from . models import regs
+
+
+# Create your views here.
+def login(request):
+    if request.method=="POST":
+        user_name=request.POST['user_name']
+        password=request.POST['password']
+        obj=auth.authenticate(user_name=user_name,password=password)
+        if obj is not None:
+            auth.login(request,obj)
+            return redirect('/')
+        else:
+            return redirect('login')
+
+    else:
+        return render(request,'login.html')
+        
+
+
+
+def register(request):
+    if request.method=="POST":
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        user_name=request.POST.get('user_name')
+        password1=request.POST.get('password1')
+        password2=request.POST.get('password2')
+        email=request.POST.get('email')
+        if password1==password2:
+            obj=regs(user_name=user_name,password=password1,email=email,first_name=first_name,last_name=last_name)
+            obj.save()
+            print("user created")
+        else:
+            print("password not matched")
+        return redirect('/')
+    else:
+        
+        return render(request,'registration.html')
